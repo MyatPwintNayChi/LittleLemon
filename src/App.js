8a503b16ae9d77e328ba8bb72600fcc659b9a25d
MyatPwintNayChi <dsilvaangelin99@gmail.com>
2024-10-29 00:21:30 +0700

    Adding files and og protocol

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "./Header";
+import Nav from "./Nav";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
-  return <div>HomePage</div>;
+  return (
+    <div className="main">
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import logo from "./images/Logo.jpg";
+function Header() {
+  return (
+    <div>
+      <img src={logo} alt="LittleLemon_Logo" width={200} />
+    </div>
+  );
+}
+
+export default Header;
